6a983ee0ed9e10edda60b5713ec6181c28f67f51
RoselineDC <roseline.danga@gmail.com>
2025-06-12 07:57:58 +0200

    modified by Roseline on 2025-06-12 07:57:58

--- frontendDeveloper4/javascript/passengercounterapp/index.js
+++ frontendDeveloper4/javascript/passengercounterapp/index.js
@@ -24,5 +24,5 @@ console.log("Bonus points:", bonusPoints);
 bonusPoints = bonusPoints - 75;
 console.log("Decremented bonus points:", bonusPoints);
 // increment the bonus points to 75
-bonusPoints = bonusPoints + 75;
+bonusPoints = bonusPoints + 5;
 console.log("Final bonus points:", bonusPoints);
