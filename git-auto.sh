b7fb11380893a42eb3aed4754c0a0ad8ec4bf360
RoselineDC <roseline.danga@gmail.com>
2025-06-12 06:51:33 +0200

    auto git commits

---
+++ git-auto.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -x
+
+
+if ! git config --global user.email > /dev/null; then
+	    git config --global user.email "roseline.danga@gmail.com"
+fi
+
+if ! git config --global user.name > /dev/null; then
+	    git config --global user.name "RoselineDC"
+fi
+
+if git diff --quiet && git diff --cached --quiet; then
+	    echo "✅ Nothing to commit."
+	        exit 0
+fi
+
+timestamp=$(date "+%Y-%m-%d %H:%M:%S")
+
+git add .
+git commit -m "modified by Roseline on $timestamp"
+
+branch=$(git symbolic-ref --short HEAD)
+git push origin "$branch"
