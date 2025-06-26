28c601b098442a6c8a7aca6b34fc09ad0d5e4f51
RoselineDC <roseline.danga@gmail.com>
2025-06-26 22:17:42 +0200

    modified by Roseline on 2025-06-26 22:17:42

--- frontendDeveloper4/javascript/blackjack/index.js
+++ frontendDeveloper4/javascript/blackjack/index.js
@@ -5,5 +5,8 @@ let secondCard = Math.floor(Math.random() * 10) + 2;
 // create variable sum set it to sum of two cards
 let sum = firstCard + secondCard;
 
-//check if sum o
+//check if sum is less than 21
+if(sum < 21){
+    console.log("Do you want to draw a new card? Your cards are " + firstCard + " and " + secondCard + ". Your current score is " + sum);
+}
 
