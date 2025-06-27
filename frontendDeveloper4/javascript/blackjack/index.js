933492618f258110003e05ea7722c1ede29e2823
RoselineDC <roseline.danga@gmail.com>
2025-06-27 07:44:53 +0200

    modified by Roseline on 2025-06-27 07:44:53

--- frontendDeveloper4/javascript/blackjack/index.js
+++ frontendDeveloper4/javascript/blackjack/index.js
@@ -18,6 +18,7 @@ if(sum < 21){
     
 }
 else if(sum === 21){
+    
     console.log("You win! Your cards are " + firstCard + " and " + secondCard + ". Your current score is " + sum);
     hasBlackJack = true;
 }
