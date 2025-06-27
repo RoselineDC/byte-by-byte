d1971ce7a1a73168ee27ce8320886d09f4704401
RoselineDC <roseline.danga@gmail.com>
2025-06-27 07:44:14 +0200

    modified by Roseline on 2025-06-27 07:44:14

--- frontendDeveloper4/javascript/blackjack/index.js
+++ frontendDeveloper4/javascript/blackjack/index.js
@@ -14,7 +14,7 @@ let message = "";
 //check if sum is less than 21
 if(sum < 21){
     message = "Do you want to draw a new card? Your cards are " + firstCard + " and " + secondCard + ". Your current score is " + sum;
-    console.log();
+    console.log(message);
     
 }
 else if(sum === 21){
