801819fdc5ae5fa1cf0efe02f78fae5beb6ad17a
RoselineDC <roseline.danga@gmail.com>
2025-06-27 07:35:56 +0200

    modified by Roseline on 2025-06-27 07:35:56

--- frontendDeveloper4/javascript/blackjack/index.js
+++ frontendDeveloper4/javascript/blackjack/index.js
@@ -5,17 +5,18 @@ let secondCard = Math.floor(Math.random() * 10) + 2;
 // create variable sum set it to sum of two cards
 let sum = firstCard + secondCard;
 
+//  check if hasBlackJack
+let hasBlackJack = false;
+
 //check if sum is less than 21
 if(sum < 21){
     console.log("Do you want to draw a new card? Your cards are " + firstCard + " and " + secondCard + ". Your current score is " + sum);
 }
 else if(sum === 21){
     console.log("You win! Your cards are " + firstCard + " and " + secondCard + ". Your current score is " + sum);
+    hasBlackJack = true;
 }
 else{
     console.log("You lose! Your cards are " + firstCard + " and " + secondCard + ". Your current score is " + sum);   
 }
 
-let age = Math.floor(Math.random() * 100) + 1;
-let canVote = age >= 18 ? "You can vote" : "You cannot vote";
-console.log(canVote);
