48b4928b5d5e086e6647afaacc0f31dbca9c3859
RoselineDC <roseline.danga@gmail.com>
2025-06-29 17:43:19 +0200

    modified by Roseline on 2025-06-29 17:43:18

--- frontendDeveloper4/javascript/blackjack/index.js
+++ frontendDeveloper4/javascript/blackjack/index.js
@@ -12,7 +12,7 @@ let isAlive = true;
 let message = "";
 // get start button
 let messageEl = document.getElementById("message-el");
-let sumEl = document.getElementById("sum-el");
+let sumEl = document.
 function startGame() {
   //check if sum is less than 21
   if (sum < 21) {
