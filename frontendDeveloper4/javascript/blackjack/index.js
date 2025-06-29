b06ca0f24f5c7ed9665f16668c8a480f93567e2d
RoselineDC <roseline.danga@gmail.com>
2025-06-29 19:50:52 +0200

    modified by Roseline on 2025-06-29 19:50:52

--- frontendDeveloper4/javascript/blackjack/index.js
+++ frontendDeveloper4/javascript/blackjack/index.js
@@ -3,7 +3,7 @@ let firstCard = Math.floor(Math.random() * 10) + 2;
 let secondCard = Math.floor(Math.random() * 10) + 2;
  let addedCard = Math.floor(Math.random() * 10) + 2;
 //create array to handle all cards
-let cards = [firstCard, secondCard, ];
+let cards = [firstCard, secondCard, addedCard];
 // create variable sum set it to sum of two cards
 let sum = firstCard + secondCard;
 
