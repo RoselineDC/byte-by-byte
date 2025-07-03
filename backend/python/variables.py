dcedc03e7f57450154e515814d0cc2dee6507b88
RoselineDC <roseline.danga@gmail.com>
2025-07-03 23:10:58 +0200

    modified by Roseline on 2025-07-03 23:10:58

--- backend/python/variables.py
+++ backend/python/variables.py
@@ -1,2 +1,31 @@
 # variables are assigned a value using the assignment operator '='
 # f-strings are used to format strings with variables
+# example:
+
+# string variable
+# strings are enclosed in single or double quotes
+name = "Roseline"
+print(f"My name is {name}.") # Output: My name is Roseline.
+
+# integer variable
+# integers are whole numbers without decimal points
+# arithmetic operations can be performed on integers
+age = 25
+print(f"I am {age} years old.") # Output: I am 25 years old
+
+# float variable
+# floats are numbers with decimal points
+# example is a price
+height = 5.5
+price = 19.99
+print(f"The price of the item is ${price}.") # Output: The price of the item is $19.99.
+print(f"My height is {height} feet.") # Output: My height is 5.5 feet.
+
+# boolean variable
+# booleans can be True or False
+# first letter is capitalized
+# USED INTERNALLY FOR LOGIC
+is_employed = False
+print(f"Is the person employed? {is_employed}.") # Output: Is the person employed? True.
+is_student = True
+print(f"Is the person a student? {is_student}.") # Output: Is the person a student? True.
