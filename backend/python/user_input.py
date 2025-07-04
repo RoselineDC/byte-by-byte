ce81090434de0eba7506f7db5b8acedddb3fa521
RoselineDC <roseline.danga@gmail.com>
2025-07-04 21:28:19 +0200

    modified by Roseline on 2025-07-04 21:28:18

--- backend/python/user_input.py
+++ backend/python/user_input.py
@@ -2,3 +2,6 @@
 # tell user what to type in and usually in quotesR
 input("Please enter your input:")
 #  CAN BE USED AS A VALUE OF A VARIABLE
+
+names = input("Please enter your names separated by commas: ")
+names_list = names.split(",")
